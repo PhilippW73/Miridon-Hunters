3b70e3d3223130f9202992de08be19f2bb42aa31
Priya Polla <priyapolla@Priyas-MacBook-Pro.local>
2018-01-29 15:00:38 -0500

    Working Data Models

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,15 +3,15 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-  app.get("/api/posts/", function(req, res) {
-    db.Post.findAll({})
+  app.get("/api/class", function(req, res) {
+    db.Class.findAll({})
     .then(function(dbPost) {
       res.json(dbPost);
     });
   });
 
-  app.get("/api/posts/character/:class", function(req, res) {
-    db.Post.findAll({
+  app.get("/api/character/:class", function(req, res) {
+    db.Character.findAll({
         where : {
           class_name : req.params.class
         }
@@ -21,36 +21,36 @@ module.exports = function(app) {
     });
   });
 
-  app.post("/api/user/posts", function(req, res) {
-    db.Post.create(
-    {
-        user_name : req.body.user_name,
-        password : req.body.password,
-        user_bio : req.body.user_bio,
-        profile_image : req.body.profile_image
-    })
-    .then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
+  // app.post("/api/user/", function(req, res) {
+  //   db.Post.create(
+  //   {
+  //       user_name : req.body.user_name,
+  //       password : req.body.password,
+  //       user_bio : req.body.user_bio,
+  //       profile_image : req.body.profile_image
+  //   })
+  //   .then(function(dbPost) {
+  //     res.json(dbPost);
+  //   });
+  // });
 
-  app.post("/api/character/posts", function(req, res) {
-    db.Post.create(
-    {
-        character_name : req.body.character_name,
-        character_descy : req.body.character_desc,
-        class_name : req.body.class_name,
-        character_image : req.body.character_image,
-        strength_point : req.body.strength_point,
-        speed_point : req.body.speed_point,
-        skill_point : req.body.skill_point,
-        ghost_hp : req.body.ghost_hp,
-        skills : req.body.skills
-    })
-    .then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
+  // app.post("/api/character/", function(req, res) {
+  //   db.Post.create(
+  //   {
+  //       character_name : req.body.character_name,
+  //       character_descy : req.body.character_desc,
+  //       class_name : req.body.class_name,
+  //       character_image : req.body.character_image,
+  //       strength_point : req.body.strength_point,
+  //       speed_point : req.body.speed_point,
+  //       skill_point : req.body.skill_point,
+  //       ghost_hp : req.body.ghost_hp,
+  //       skills : req.body.skills
+  //   })
+  //   .then(function(dbPost) {
+  //     res.json(dbPost);
+  //   });
+  // });
 
   // app.put("/api/user/:username", function(req, res) {
   //   db.Post.update({
--- schema.sql
+++ schema.sql
@@ -34,7 +34,7 @@ CREATE TABLE classes(
     classes_id INT NOT NULL AUTO INCREMENT,
     class_name VARCHAR(100) NOT NULL,
     classs_desc VARCHAR(400),
-    image VARCHAR(100) NOT NULL,
+    class_image VARCHAR(100) NOT NULL,
     strength_point INT NOT NULL,
     speed_point INT NOT NULL,
     skill_point INT NOT NULL,
@@ -42,3 +42,10 @@ CREATE TABLE classes(
     skills VARCHAR(300),
     PRIMARY KEY (classes_id)
 )
+
+
+INSERT INTO Classes(class_name, class_desc, class_image, strength_point, speed_point, skill_point, ghost_hp, createdAt, updatedAt) VALUES ("Class1", "ABCDABCD", "http://google.com", 100, 90, 40, 40, current_timestamp(), current_timestamp());
+INSERT INTO Classes(class_name, class_desc, class_image, strength_point, speed_point, skill_point, ghost_hp, createdAt, updatedAt) VALUES ("Class2", "ZZZZZZZZZZZZZZZZ", "http://google.com", 80, 80, 60, 60, current_timestamp(), current_timestamp());
+
+INSERT INTO Characters(character_name, character_desc, class_name, character_image, strength_point, speed_point, skill_point, ghost_hp, createdAt, updatedAt) VALUES ("Character11", "Char1", "Class1", "http://google.com", 100, 90, 40, 40, current_timestamp(), current_timestamp());
+INSERT INTO Characters(character_name, character_desc, class_name, character_image, strength_point, speed_point, skill_point, ghost_hp, createdAt, updatedAt) VALUES ("Character12", "Char2", "Class1", "http://google.com", 80, 100, 40, 80, current_timestamp(), current_timestamp());
