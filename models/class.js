7bf509dae755b54f39c4953bf053826fe385cf75
Joonho Lee <joonholee@Joonhos-MacBook-Air.local>
2018-01-30 10:50:41 -0500

    deleted apps folder

---
+++ models/class.js
@@ -0,0 +1,43 @@
+module.exports =  function(squelize, DataTypes){
+
+    var Class = squelize.define("Class", {
+
+        class_id : {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        class_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        class_desc: {
+            type: DataTypes.TEXT
+        },
+        class_image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        strength_point : {
+            type : DataTypes.INTEGER,
+            allowNull: false
+        }, 
+        speed_point : {
+            type : DataTypes.INTEGER,
+            allowNull: false
+        },
+        skill_point : {
+            type : DataTypes.INTEGER,
+            allowNull: false
+        },
+        ghost_hp : {
+            type : DataTypes.INTEGER,
+            allowNull: false
+        },
+        skills : {
+            type: DataTypes.TEXT
+        }
+    });
+    return Class;
+};
