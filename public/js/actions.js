6183375329d7f9cd1136d9c11b7e7e09b9a352e6
Joonho Lee <joonholee@Joonhos-MacBook-Air.local>
2018-01-31 15:35:34 -0500

    actions.js

---
+++ public/js/actions.js
@@ -0,0 +1,91 @@
+function restoreStrength(who, func){
+retoreValue <= player.strengthPoint
+retoreValue = (player.strengthPoint + 2) + Math.abs(player.fullStat.strengthPoint%5)
+if(player.movement === "Restore Speed Points"){
+    retoreValue = (player.strengthPoint+3) + Math.abs((player.fullStat.strengthPoint%5)*2) 
+}
+$("#comments p").text("Player restored" + player.strengthPoint + retoreValue + " Strength Points");
+}
+
+function meleeAttack(who, func){
+    
+    if(who === 0){
+       enemy.hitPoint =  enemy.hitPoint - Math.floor(Math.random()*6)+1
+       player.strength = player.strength - 2
+       player.speed = player.speed - 1 
+       $("#comments p").text("Player's attacked dealt" + enemy.hitPoint - Math.floor(Math.random()*6)+1 + " damage");
+}
+    }
+    if(who === 1){
+        player.hitPoint = player.hitPoint - Math.floor(Math.random()*6)+1
+        enemy.strength = enemy.strength - 2
+        enemy.speed = enemy.speed - 1
+        $("#comments p").text("Enemy's attacked dealt" + player.hitPoint - Math.floor(Math.random()*6)+1 + " damage");
+    }
+
+}
+
+function meleeCombo(who, func){
+    if(player.move === meleeAttack){
+        if(who === 0){
+            enemy.hitPoint = enemy.hitPoint - Math.floor(Math.random()*6)+1
+            player.strength = player.strength - 1
+            player.speed = player.speed - 2
+            $("#comments p").text("Player's attacked dealt" + enemy.hitPoint - Math.floor(Math.random()*6)+1 + " damage");
+        }
+        if(who === 1){
+            player.hitPoint = player.hitPoint - Math.floor(Math.random()*6)+1
+            enemy.strength = enemy.strength - 1
+            enemy.speed = enemy.speed - 2
+            $("#comments p").text("Enemy's attacked dealt" + player.hitPoint - Math.floor(Math.random()*6)+1 + " damage");
+        }
+    }
+
+}
+
+function gunAttack(who, func){
+    if(who === 0){
+        enemy.hitPoint = enemy.hitPoint - Math.floor(Math.random()*6)+1
+        player.speed = player.speed - 2
+        $("#comments p").text("Player's shot dealt" + enemy.hitPoint - Math.floor(Math.random()*6)+1 + " damage");
+        
+    }
+    if(who === 1){
+        player.hitPoint = player.hitPoint - Math.floor(Math.random()*6)+1
+        enemy.speed = enemy.speed - 2
+        $("#comments p").text("Enemy's shot dealt" + player.hitPoint - Math.floor(Math.random()*6)+1 + " damage");
+        
+    }
+}
+
+function aimedAttack(who, func){
+    if(who === 0){
+        enemy.hitPoint = enemy.hitPoint - Math.floor(Math.random()*6)+3
+        player.speed = player.speed - 3
+        $("#comments p").text("Player's aimed shot dealt" + enemy.hitPoint - Math.floor(Math.random()*6)+3 + " damage");
+        
+    }
+    if(who === 1){
+        player.hitPoint = player.hitPoint - Math.floor(Math.random()*6)+3
+        enemy.speed = enemy.speed - 3
+        $("#comments p").text("Enemy's aimed shot dealt" + player.hitPoint - Math.floor(Math.random()*6)+3 + " damage");
+        
+    }
+}
+
+function reload(who, func){
+
+}
+
+function restoreSpeed(who, func){
+    retoreValue <= player.speedPoint
+    retoreValue = (player.speedPoint + 2) + Math.abs(player.fullStat.speedPoint%5)
+    if(player.movement === "Restore Strength Points"){
+        retoreValue = (player.speedPoint+3) + Math.abs((player.fullStat.speedPoint%5)*2) 
+    }
+    $("#comments p").text("Player restored" + player.speedPoint + retoreValue + " Speed Points");
+    }
+
+    functiom block(who, func){
+        
+    }
