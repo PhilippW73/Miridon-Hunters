de3fef2daf28be1f51de841812ffdd3a1cf36012
abruestle <anna@bruestle.net>
2018-02-16 14:38:48 -0500

    action utils updated to have previous code as comment

--- src/utils/actions/restoreSpeed.js
+++ src/utils/actions/restoreSpeed.js
@@ -4,7 +4,7 @@ export default {
   restoreSpeed: function(who){
     let restoreValue = Math.min((who.curStats.speed_point + 2) + Math.floor(who.fullStats.speed_point / 5), (who.fullStats.speed_point - who.curStats.speed_point));
     if(who.Offensive === "Restore Strength Points"){
-        restoreValue = Math.min((who.fullStats.speed_point+3)+ Math.floor(who.fullStats.speed_point / 5)*2, (who.fullStats.speed_point - who.curStats.speed_point)); 
+        restoreValue = Math.min((who.fullStats.speed_point + 3)+ Math.floor(who.fullStats.speed_point / 5)*2, (who.fullStats.speed_point - who.curStats.speed_point)); 
     }
     return {
       result: who,
