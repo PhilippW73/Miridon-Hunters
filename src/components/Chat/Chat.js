3ee773b94d6e6a492a03b86b73e499e2b36a4c5a
Philipp Wickart <phwickart@gmail.com>
2018-02-16 15:24:28 -0500

    update components

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Chat.css";
+
+const Chat = props =>
+
+<div
---
+++ src/pages/Profile.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Image from "../components/Image";
+import Chat from "../components/Chat";
+import Navbar from "../components/Navbar";
+import Input from "../components/Input";
+import Row from "../components/Row";
+import Column from "../components/Colmun";
+import Greeting from "../components/Greeting";
+import Wins_Losses from "../components/Wins_Losses";
+
+const Profile = () =>
+  <div>
+  	
+  	<Header/>
+  		{/*image*/}
+  		
+  			<Chat/>
+  				
+  				<Greeting/>
+  				
+  					<Input/>
+  						{/*image*/}
+  								
+  						<Wins_Losses/>
+  								
+  							<Footer/>
+  									
+  		
+  </div>;
+
+  export default Profile;
