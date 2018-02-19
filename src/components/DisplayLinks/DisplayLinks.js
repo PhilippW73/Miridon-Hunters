d0789336c38beb0f172ff8664b1f15db9a3c13a4
Philipp Wickart <phwickart@gmail.com>
2018-02-19 09:45:51 -0500

    Passport Setup

---
+++ src/components/DisplayLinks/DisplayLinks.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import "./DisplayLinks.css";
+
+const DisplayLinks = props => {
+	if (props.loggedIn) {
+		return (
+			<nav className="navbar">
+				<ul className="nav">
+					<li className="nav-item">
+						<Link to="/" className="nav-link">
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link to="#" className="nav-link" onClick={props._logout}>
+							Logout
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		)
+	} else {
+		return (
+			<nav className="navbar">
+				<ul className="nav">
+					<li className="nav-item">
+						<Link to="/" className="nav-link">
+							Home
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/login" className="nav-link">
+							login
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/signup" className="nav-link">
+							sign up
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		)
+	}
+}
+export default DisplayLinks
