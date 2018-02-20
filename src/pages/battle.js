fab5fb47a1576d4915b5c4a8cc40848b202c4a1c
abruestle <anna@bruestle.net>
2018-02-20 14:42:29 -0500

    Offensive Action chooser

--- src/pages/battle.js
+++ src/pages/battle.js
@@ -144,6 +144,53 @@ class Battle extends Component {
     }
   }
 
+  //battle order functions
+    //actions
+    function chooseOffense (input) {
+      console.log(input+" is attacking");
+      let who = this.state.eval(input);
+      let target = this.state.eval(this.state.eval(input).opposition);
+      let results = {};
+      switch(this.state.eval(who).Offensive) {
+        case "Restore Strength Points":
+          results = restoreStrength(who);
+          break;
+        case "Melee Attack":
+          results = meleeAttack(who, target);
+          break;
+        case "Melee Combo Attack":
+          results = meleeCombo(who, target);
+          break;
+        case "Gun Attack":
+          results = gunAttack(who, target);
+          break;
+        case "Aimed Attack":
+          results = aimedAttack(who, target);
+          break;
+        case "Reload":
+          results = reload(who, target);
+          break;
+        default:
+          break;
+      }
+      this.setState({
+        [input]: results.who,
+        [this.state.eval(input).opposition]: results.target,
+        comments: this.state.comments + results.comment
+      });
+      //CHECK FOR SYNTAX - trying to get who's name and see if it matches first.
+      console.log("Ready for next turn part.");
+      console.log("--------------------");
+      console.log(input);
+      console.log(first);
+      if(input === first) {
+        this.checkDead(this.state.eval(who).opposition, this.chooseMove);
+      } else {
+        this.checkDead(this.state.eval(who).opposition, this.endTurn);
+      }
+    }
+
+
   //Handles choice of actions
   handleActionChange = (event) => {
     let player = this.state.player;
@@ -192,90 +239,6 @@ export default Battle;
 
 
 
-//     //battle order functions
-//       function turnOrder() {
-//         if (player.fullStats.speed_point > enemy.fullStats.speed_point) {
-//           first = "player";
-//         } else if (player.fullStats.speed_point < enemy.fullStats.speed_point || Math.random()<.5 ) {
-//           first = "enemy";
-//         } else {
-//           first = "player";
-//         }
-//       }
-//       function initializeCharacter(id, where, func) {
-//         $.get("/api/characters/" + id, function(data) {
-//           console.log(JSON.stringify(where) +": "+ JSON.stringify(data));
-//           where.fullStats = Object.assign({}, data);
-//           where.curStats = Object.assign({}, data);
-//           console.log(where.fullStats);
-//           where.fullStats.hit_point = 5*(data.strength_point + data.speed_point);
-//           func();
-//         });
-//       }
-//       function updateProgress() {
-//         // console.log("Max: "+JSON.stringify(player.fullStats));
-//         // console.log("Current: "+JSON.stringify(player.curStats));
-//         for(i = 0; i < stats.length; i++){
-//           // console.log(stats[i]+": "+parseFloat(player.curStats[stats[i]]));
-//           // console.log(parseFloat(player.curStats[stats[i]]));
-//           // console.log(parseFloat(player.fullStats[stats[i]]));
-//           // console.log(parseFloat(player.curStats[stats[i]])/parseFloat(player.fullStats[stats[i]]));
-//           // console.log("--------------");
-//           $("#"+ player.fullStats.character_id + stats[i]).attr("aria-valuenow",player.curStats[stats[i]]);
-//           $("#"+ player.fullStats.character_id + stats[i]).css('width',100*parseFloat(player.curStats[stats[i]])/parseFloat(player.fullStats[stats[i]])+"%");
-//           $("#"+ player.fullStats.character_id + stats[i]+ "Text").text(player.curStats[stats[i]]);
-//           $("#"+ enemy.fullStats.character_id + stats[i]).attr("aria-valuenow", enemy.curStats[stats[i]]);
-//           $("#"+ enemy.fullStats.character_id + stats[i]).css('width',100*parseFloat(enemy.curStats[stats[i]])/parseFloat(enemy.fullStats[stats[i]])+"%");
-//           $("#"+ enemy.fullStats.character_id + stats[i]+ "Text").text(enemy.curStats[stats[i]]);
-//         }
-
-//       }
-//       function initialize() {
-//         console.log("Initalizing "+ $("#player").attr('value') + " as player.");
-//         initializeCharacter( $("#player").attr('value'), player, initialize2);
-//       }
-//       function initialize2() {
-//         console.log("Initalizing "+$("#enemy").attr('value') + " as enemy.");
-//         initializeCharacter( $("#enemy").attr('value'), enemy, turnOrder);
-//       }
-//       //actions
-//       function chooseOffense (who) {
-//         console.log(who+" is attacking");
-//         switch(eval(who).Offensive) {
-//           case "Restore Strength Points":
-//             restoreStrength(who);
-//             break;
-//           case "Melee Attack":
-//             meleeAttack(who);
-//             break;
-//           case "Melee Combo Attack":
-//             meleeCombo(who);
-//             break;
-//           case "Gun Attack":
-//             gunAttack(who);
-//             break;
-//           case "Aimed Attack":
-//             aimedAttack(who);
-//             break;
-//           // case "Reload":
-//           //   reloadFunc(who);
-//           //   break;
-//           default:
-//             break;
-//         }
-//             //no offensive action taken
-
-//         //CHECK FOR SYNTAX - trying to get who's name and see if it matches first.
-//         console.log("Ready for next turn part.");
-//         console.log("--------------------");
-//         console.log(who);
-//         console.log(first);
-//         if(who === first) {
-//           checkDead(eval(who).opposition, chooseMove);
-//         } else {
-//           checkDead(eval(who).opposition, endTurn);
-//         }
-//       }
 //       function chooseDefense (who) {
 //         console.log(who+" is defending");
 //         switch(eval(who).Defensive) {
