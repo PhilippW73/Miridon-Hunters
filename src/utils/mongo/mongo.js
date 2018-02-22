a1a84d492151f956cc8ad20faaa5562c1a586ebd
abruestle <anna@bruestle.net>
2018-02-22 11:59:37 -0500

    wins/losses mongoose

--- src/utils/mongo/mongo.js
+++ src/utils/mongo/mongo.js
@@ -3,9 +3,19 @@ import mongoose from "mongoose";
 
 export default {
   getCharacter: function(id) {
-    db.Characters.findOne({ _id: id })
+    db.Characters.findOne({ character_id: id })
     .then(function(dbCharacter) {
       return res.json(dbCharacter);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      return res.json(err);
+    });
+  },
+  getUser: function(id) {
+    db.Users.findOne({ user_id: id })
+    .then(function(dbUser) {
+      return res.json(dbUser);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -49,8 +59,41 @@ export default {
   getActions: function(id, strengthpoints, speedpoints) {
     this.getCharacter(id)
       .then(function(dbCharacter){
+        //weapon types for all weapons
         switch (dbCharacter.weapon){
+          case "small shield":
+          case "large shield":
+          case "great shield":
+          case "grand shield":
+          case "monument shield":
+          case "shield":
+            const weapontype = "shield";
+            break;
           case "ranged":
+          case "palm pistol":
+          case "revolver":
+          case "heavy revolver":
+          case "hand cannon":
+          case "dreadnaught":
+          case "hunting rifle":
+          case "battle rifle":
+          case "heavy rifle":
+          case "targeting cannon":
+          case "hand howitzer":
+          case "shotgun":
+          case "spreadshot":
+          case "scattershot":
+          case "iron mist":
+          case "bullet rain storm":
+          case "stun pistol":
+          case "shock gun":
+          case "plasma rifle":
+          case "battery arm":
+          case "plasma cannon":
+          case "sniper rifle":
+          case "tesla":
+          case "shotgun":
+          case "pistol":
             const weapontype = "ranged";
             break;
           case "melee":
@@ -59,6 +102,27 @@ export default {
           case "greatsword":
           case "grand saber":
           case "monument sword":
+          case "sword":
+          case "hand drill":
+          case "arm drill":
+          case "power drill":
+          case "grand drill":
+          case "star drill":
+          case "heaven piercing drill":
+          case "hand hammer":
+          case "war hammer":
+          case "maul":
+          case "great maul":
+          case "the crusher":
+          case "unarmed":
+          case "claw":
+          case "clawed gauntlet":
+          case "robot arm":
+          case "dragon claw":
+          case "mech arm":
+          case "drill":
+          case "hammer":
+          case "grappler":
           default:
             const weapontype = "melee";
             break;
@@ -77,42 +141,94 @@ export default {
         });
       });
   },
-  
-
-
-  //   mongo.getActionTypes()
-  //   mongo.getActions(id, strength, speed)
-  //   mongo.charLose(this.state.player.character_id);
-  //       mongo.charWin(this.state.enemy.character_id);
-  //       mongo.playerLose(id);
-  //       mongo.playerWin(id);
-
-
-
-  // getRandomDog: function() {
-  //   return axios.get("https://dog.ceo/api/breeds/image/random");
-  // },
-  // getDogsOfBreed: function(breed) {
-  //   return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  // },
-  // getBaseBreedsList: function() {
-  //   return axios.get("https://dog.ceo/api/breeds/list");
-  // }
+  createUser: function() {
+    db.User.create(
+      {
+          email : req.body.email,
+          password : req.body.password,
+          username : req.body.username,
+          user_bio : req.body.user_bio,
+          profile_image : req.body.profile_image
+      })
+      .then(function(dbUser) {
+        res.redirect(307, "/login");
+      }).catch(function(err) {
+        console.log(err);
+        return res.json(err);
+      });
+  },
+  updateUser: function(id, params) {
+    db.User.update(
+      {
+        user_id: id
+      }, {
+        $set: {
+          params
+        }
+      })
+      .then(function(dbUser) {
+        return res.json(dbUser);
+      }).catch(function(err) {
+        console.log(err);
+        return res.json(err);
+      });
+  },
+  charLose: function(id) {
+    db.Character.findOneAndUpdate(
+      {
+        character_id: id
+      }, {
+        $inc: { losses: 1 }
+      }, options, callback)
+      .then(function(dbCharacter) {
+        return res.json(dbCharacter);
+      }).catch(function(err) {
+        console.log(err);
+        return res.json(err);
+      });
+  },
+  playerLose: function(id) {
+    db.User.findOneAndUpdate(
+      {
+        user_id: id
+      }, {
+        $inc: { losses: 1 }
+      }, options, callback)
+      .then(function(dbCharacter) {
+        return res.json(dbCharacter);
+      }).catch(function(err) {
+        console.log(err);
+        return res.json(err);
+      });
+  },
+  charWin: function(id) {
+    db.Character.findOneAndUpdate(
+      {
+        character_id: id
+      }, {
+        $inc: { wins: 1 }
+      }, options, callback)
+      .then(function(dbCharacter) {
+        return res.json(dbCharacter);
+      }).catch(function(err) {
+        console.log(err);
+        return res.json(err);
+      });
+  },
+  playerWin: function(id) {
+    db.User.findOneAndUpdate(
+      {
+        user_id: id
+      }, {
+        $inc: { wins: 1 }
+      }, options, callback)
+      .then(function(dbCharacter) {
+        return res.json(dbCharacter);
+      }).catch(function(err) {
+        console.log(err);
+        return res.json(err);
+      });
+  }
 };
 
-
-db.User.create(
-  {
-      email : req.body.email,
-      password : req.body.password,
-      username : req.body.username,
-      user_bio : req.body.user_bio,
-      profile_image : req.body.profile_image
-  })
-  .then(function(dbUser) {
-    res.redirect(307, "/login");
-  }).catch(function(err) {
-    console.log(err);
-    res.json(err);
-  });
 
