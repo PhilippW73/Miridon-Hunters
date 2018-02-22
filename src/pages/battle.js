a070d12b8cc1e055a637d918e4cd352be6636859
Joonho Lee <joonholee@Joonhos-MacBook-Air.local>
2018-02-22 11:04:50 -0500

    updated battle.js

--- src/pages/battle.js
+++ src/pages/battle.js
@@ -6,16 +6,41 @@ import Chat from "../components/Chat";
 import Navbar from "../components/Navbar";
 import Input from "../components/Input";
 import Row from "../components/Row";
-import Column from "../components/Colmun";
+import Col from "../components/col";
 import Greeting from "../components/Greeting";
 import Wins_Losses from "../components/Wins_Losses";
 
+//import API from "../utils/API";
+//import mongo from "../utils/mongo";
+//import Container from "../components/Container";
+import {
+  aimedAttack,
+  block,
+  charge,
+  dodge,
+  gunAttack,
+  meleeAttack,
+  meleeCombo,
+  reload,
+  restoreSpeed,
+  restoreStrength
+} from "../utils/actions";
+
+const stats = [
+  {name: "Hit Points",
+  reference: "hit_point",
+  progressClass: "bg-danger"},
+  {name: "Strength",
+  reference: "strength_point",
+  progressClass: "bg-warning"},
+  {name: "Speed",
+  reference: "speed_point",
+  progressClass: "bg-success"},
+  {name: "Ghost Hit Points",
+  reference: "ghost_hp",
+  progressClass: ""}];
 
 
-
-<<<<<<< HEAD
-export default Battle;
-=======
 class Battle extends Component {
   state = {
     error: "",
@@ -35,10 +60,8 @@ class Battle extends Component {
       Defensive: "",
       actions: {}
     },
-    actionTypes: [],
+    actionTypes: {},
     first: "",
-    meleeCombo: false,
-    actionsDisabled: false;
     comments: "Choose your actions for the round (one of each), and then press 'Start Turn'."
   };
 
@@ -61,19 +84,7 @@ class Battle extends Component {
         this.setState({
           player: player
         })
-        this.getActionTypes();
-      })
-      .catch(err => console.log(err));
-  }
-
-  getActionTypes() {
-    mongo.getActionTypes()
-      .then(res => {
-        //Send action type info 
-        this.setState({
-          actionTypes: res.data.message
-        })
-        this.getActions("player", this.getEnemy);
+        this.getEnemy();
       })
       .catch(err => console.log(err));
   }
@@ -91,13 +102,23 @@ class Battle extends Component {
         this.setState({
           enemy: enemy
         })
-        this.initiative();
-        this.getActions("enemy", );
+        this.getActions();
       })
       .catch(err => console.log(err));
   }
 
-  getActions(who, callback) {
+  getActionTypes() {
+    mongo.getActionTypes()
+      .then(res => {
+        //Send action type info 
+        this.setState({
+          actionTypes: res.data.message
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  getActions(who) {
     //sends id based on who
     const id = this.state.eval(who).fullStats.character_id;
     const strength = this.state.eval(who).curStats.strength_point;
@@ -107,15 +128,10 @@ class Battle extends Component {
         let temp = this.state.eval(who);
         temp.actions = res.data.message;
         this.setState({
-          [who].actions.Offensive: temp.Offensive,
-          [who].actions.Movement: temp.Movement,
-          [who].actions.Defensive: temp.Defensive
+          [who]: temp
         })
       })
       .catch(err => console.log(err));
-    if(callback && typeof callback === "function"){
-      callback();
-    }
   }
 
   initiative = () => {
@@ -141,15 +157,9 @@ class Battle extends Component {
           break;
         case "Melee Attack":
           results = meleeAttack(who, target);
-          if (input === "player"){
-            this.setState({meleeCombo: true});
-          }
           break;
         case "Melee Combo Attack":
           results = meleeCombo(who, target);
-          if (input === "player"){
-            this.setState({meleeCombo: true});
-          }
           break;
         case "Gun Attack":
           results = gunAttack(who, target);
@@ -222,104 +232,224 @@ class Battle extends Component {
       chooseDefense(who.opposition);
       chooseOffense(input);
     }
-  //Enemy Choice
-    function enemyChoice(){
-      this.getActions("enemy", () =>{
-        this.setState({
-          enemy.Offensive: (this.state.enemy.actions.Offensive[Math.floor(Math.random()*this.state.enemy.actions.Offensive.length + 1 )]),
-          enemy.Movement: (this.state.enemy.actions.Movement[Math.floor(Math.random()*this.state.enemy.actions.Movement.length + 1 )]),
-          enemy.Defensive: (this.state.enemy.actions.Defensive[Math.floor(Math.random()*this.state.enemy.actions.Defensive.length + 1 )])
-        });
-        console.log("Enemy choices:"+this.state.enemy.Movement+this.state.enemy.Offensive+this.state.enemy.Defensive);
-        if(this.state.first === "player"){
-          //player goes first
-          this.chooseMove("player");
-        } else {
-          //enemy goes first
-          this.chooseMove("enemy");
-        }
-      });
-    }
 
-  function checkDead (who, func) {
-    if(this.state.eval(who).curStats.hit_point < 1) {
-      $(".dropdown-toggle").addClass("disabled");
-      $("#startTurn").addClass("disabled");
-      if (who === "player") {
-        //lose
-        this.setState({comments: this.state.comments + " You lost the battle."});
-        mongo.charLose(this.state.player.character_id);
-        mongo.charWin(this.state.enemy.character_id);
-        mongo.playerLose(id);
-      } else {
-        //win
-        this.setState({comments: this.state.comments + " You won the battle!"});
-        mongo.charWin(this.state.player.character_id);
-        mongo.charLose(this.state.enemy.character_id);
-        mongo.playerWin(id);
-      }
-    } else {
-      func(who);
-    }
-  }
 
-  function endTurn () {
-    console.log("Turn end");
-    //set buttons back to normal
-    this.setState({
-      actionsDisabled: false
-    });
-  }
+    //Handles choice of actions
+handleActionChange = (event) => {
+  let player = this.state.player;
+  player[event.target.name] = event.target.value;
+  this.setState({ player: player});
+}
 
-  //Handles choice of actions
-  handleActionChange = (event) => {
-    let player = this.state.player;
-    player[event.target.name] = event.target.value;
-    this.setState({ player: player});
-  }
+handleFormSubmit = event => {
+  event.preventDefault();
+  //On submit... 1. Make enemy actions up 2. Figure out who goes first 3. Run through actions
+};
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    this.setState({
-      meleeCombo: false,
-      actionsDisabled: true,
-      comments: ""
-    });
-    this.enemyChoice();
-  };
-
-  render() {
-    return (
-      <Container>
-        <Header />
-        <Row>
-          <Col size="md-offset-1 md-4">
-            <BattleChar {...this.state.player} stats={stats} />
-          </Col>
-          <Col size="md-offset-1 md-4">
-            <BattleChar {...this.state.enemy} stats={stats} />
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <h3>Actions</h3>
-            <div className="btn-group" role="group">
-              {props.actionTypes.map(actionType => <ButtonDropdown actionType actions strength={this.state.player.curStats.strength_point} speed={this.state.player.curStats.speed_point} weapon={this.state.player.fullStats.weapon} current={this.state.player[actionType.name]} meleeCombo actionsDisabled/>
-                )}
-              <button class="btn btn-default" type="button" id="startTurn" onClick={this.handleActionChange} { this.state.actionsDisabled ? "disabled" : "" }>Start Turn
-              </button> 
-            </div>
-            <div>{this.state.comments}
-            </div>
-            <a class="btn btn-default" href="http://miridon.reuniontechnologies.com/page/battlerules-actions" role="button" target="_blank">See All Actions</a>
-          </Col>
-        </Row>
-        <Footer />
-      </Container>
-    );
-  }
+render() {
+  return (
+    <Container>
+      <Header />
+      <Row>
+        <Col size="md-offset-1 md-4">
+          <BattleChar {...this.state.player} stats={stats} />
+        </Col>
+        <Col size="md-offset-1 md-4">
+          <BattleChar {...this.state.enemy} stats={stats} />
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-12">
+          <h3>Actions</h3>
+          <div className="btn-group" role="group">
+            {props.actionTypes.map(actionType => <ButtonDropdown actionType actions strength={this.state.player.curStats.strength_point} speed={this.state.player.curStats.speed_point} weapon={this.state.player.fullStats.weapon}/>
+              )}
+            <button class="btn btn-default" type="button" id="startTurn">Start Turn
+            </button> 
+          </div>
+          <div>{this.state.comments}
+          </div>
+          <a class="btn btn-default" href="http://miridon.reuniontechnologies.com/page/battlerules-actions" role="button" target="_blank">See All Actions</a>
+        </Col>
+      </Row>
+      <Footer />
+    </Container>
+  );
+}
 }
 
 export default Battle;
 
->>>>>>> 0d77621220dd9365c20ee2ad329915dd1b2a5a49
+
+
+
+//       function enemySingleChoice (i){
+//         $.get("/api/actions/availableByType/"+actionTypes[i]+"/"+enemy.curStats.strength_point+"/"+enemy.curStats.speed_point, function(data) {
+//           //console.log("Possible enemy actions: "+ JSON.stringify(data));
+//           //if data exists
+//           if(data[0]){
+//             enemy[actionTypes[i]] = (data[Math.floor(Math.random()*data.length)].name);
+//           }
+//           if(i >= actionTypes.length - 1) {
+//             enemyChoiceEnd();
+//           } else {
+//             enemySingleChoice(i+1);
+//           }
+//         });
+//       }
+//       function enemyChoiceEnd() {
+//         console.log("Enemy choices: "+JSON.stringify(enemy, null, 2));
+//         console.log("Finished:"+enemy.Movement+enemy.Offensive+enemy.Defensive);
+//         if(first === "player"){
+//           //player goes first
+//           chooseMove(player.position);
+//         } else {
+//           //enemy goes first
+//           chooseMove(enemy.position);
+//         }
+//       }
+//       function enemyChoice () {
+//         //console.log(JSON.stringify(enemy.curStats, null, 2));
+//         var fullFunction
+//         for (i = 0; i < actionTypes.length; i++){
+//           //blanks it out so async functions can run all together
+//           enemy[actionTypes[i]] = "";
+//         }
+//         enemySingleChoice(0);
+
+//         //IF PROMISES (Not working yet so commented out)
+//           // var promises = [];
+//           // for (i = 0; i < actionTypes.length; i++){
+//           //   promises[i] = $.get("/api/actions/availableByType/"+actionTypes[i]+"/"+enemy.curStats.strength_point+"/"+enemy.curStats.speed_point, function(data) {
+//           //     //console.log("Possible enemy actions: "+ JSON.stringify(data));
+//           //     //if data exists
+//           //     if(data[0]){
+//           //       enemy[actionTypes[i]] = (data[Math.floor(Math.random()*data.length)]);
+//           //     }
+//           //     // if(actionTypes[i]="Movement"){
+//           //     //   //figure out turn order
+
+//           //     // }
+//           //   });
+//           // }
+
+//           // // await Promise.all(promises);
+
+//           // // var promise1 = Promise.resolve(3);
+//           // // var promise2 = 42;
+//           // // var promise3 = new Promise(function(resolve, reject) {
+//           // //   setTimeout(resolve, 100, 'foo');
+//           // // });
+
+//           // Promise.all(promises).then(function(values) {
+//           //   console.log("Enemy choices: "+JSON.stringify(enemy, null, 2));
+//           //   console.log("Finished:"+enemy.Movement+enemy.Offensive+enemy.Defensive);
+//           //   if(first === "player"){
+//           //     //player goes first
+//           //     chooseMove(player.position);
+//           //   } else {
+//           //     //enemy goes first
+//           //     chooseMove(enemy.position);
+//           //   }
+//           // });
+
+
+//       }
+//       function updateDropdownButton (type, newValue) {
+//         updateAction(player, type, newValue)
+//         if(!newValue){
+//           newValue = type;
+//         }
+//         $("#"+type+"DropdownText").text(newValue);
+//       }
+//       function updateAction (who, type, newValue) {
+//         who[type] = newValue;
+//         console.log(type + " set to "+newValue);
+//       }
+//       function startTurn () {
+//         //disable dropdowns
+//         $(".dropdown-toggle").addClass("disabled");
+//         //enemy selects
+//         console.log("dropdowns disabled");
+//         toggleCombo(true);
+//         $("#comments p").text("");
+//         updateProgress();
+//         enemyChoice();
+
+//       }
+//       function endTurn () {
+//         console.log("Turn end");
+//         //set buttons back to normal
+//         for (i = 0; i < actionTypes.length; i++){
+//           updateDropdownButton(actionTypes[i])
+//         }
+//         updateProgress();
+//         //enable dropdowns
+//         $(".dropdown-toggle").removeClass("disabled");
+//       }
+//       function checkDead (who, func) {
+//         if(eval(who).curStats.hit_point < 1) {
+//           $(".dropdown-toggle").addClass("disabled");
+//           $("#startTurn").addClass("disabled");
+//           if (who === "player") {
+//             //lose
+//             $("#comments p").append(" You lost the battle.");
+//             $.ajax({
+//               method: "PUT",
+//               url: "/api/lost/"+player.fullStats.character_id
+//             });
+//             $.ajax({
+//               method: "PUT",
+//               url: "/api/won/"+enemy.fullStats.character_id
+//             });
+//           } else {
+//             //win
+//             $("#comments p").append(" You won the battle!");
+//             $.ajax({
+//               method: "PUT",
+//               url: "/api/won/"+player.fullStats.character_id
+//             });
+//             $.ajax({
+//               method: "PUT",
+//               url: "/api/lost/"+enemy.fullStats.character_id
+//             });
+//           }
+//         } else {
+//           func(who);
+//         }
+//       }
+//       function toggleCombo(hide) {
+//         //if ($(":contains('Melee Combo Attack')").hasClass("hidden")){
+//         if(hide) {
+//           $("a:contains('Melee Combo Attack')").addClass("hidden");
+//         } else {
+//           $("a:contains('Melee Combo Attack')").removeClass("hidden");
+//         } 
+//       }
+
+//       toggleCombo(true);
+//     //Start!
+//       initialize();
+
+//     //Buttons
+
+//       $("body").on("click",".dropdown-menu li a", function(){
+//         event.preventDefault();
+
+//         // change the button and action
+//         if (!$("#startTurn").hasClass("disabled")) {
+//           console.log("Selection made");
+//           updateDropdownButton($(this).parent().parent().parent().attr("value"), $(this).text());
+//         }
+
+//       });
+//       $("body").on("click", "#startTurn", function() {
+//         event.preventDefault();
+//         if (!$("#startTurn").hasClass("disabled")) {
+//           startTurn();
+//         }
+//       });
+
+//   });
+
+export default Battle;
