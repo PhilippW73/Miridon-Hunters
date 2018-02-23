1552a60576de64c14cca20e27fa80773403d7676
Philipp Wickart <phwickart@gmail.com>
2018-02-23 14:37:51 -0500

    update bio and passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,35 @@
-const db = require("../server/db");
+const User = require("../server/db/models/user");
 
 // Defining methods for the userController
 module.exports = {
   findOne: function(req, res) {
-    db.User
+    User
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.User
+    User
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.User
+    User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.User
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+    console.log("UPDATE USER");
+    User
+      .findOneAndUpdate({ _id: req.user._id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.User
+    User
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
